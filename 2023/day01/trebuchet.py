82d370ff60095d33833c9e048c1cc07a068cf346
Osman Haji <osman6191@outlook.com>
2023-12-02 12:50:30 +1100

    added part 2 solution

--- 2023/day01/trebuchet.py
+++ 2023/day01/trebuchet.py
@@ -2,17 +2,20 @@ with open("input.txt") as file:
     lines = [line.rstrip() for line in file]
 
 
-calibration_values = [[] for _ in range(len(lines))]
+def find_calibration_value(lines):
+    calibration_values = [[] for _ in range(len(lines))]
 
-for i, line in enumerate(lines):
-    for char in line:
-        try:
-            calibration_values[i].append(int(char))
-        except:
-            pass
+    for i, line in enumerate(lines):
+        for char in line:
+            try:
+                calibration_values[i].append(int(char))
+            except:
+                pass
 
-calibration_values_sum = 0
-for i in calibration_values:
-    calibration_values_sum += int(str(i[0]) + str(i[-1]))
+    calibration_values_sum = 0
+    for i in calibration_values:
+        calibration_values_sum += int(str(i[0]) + str(i[-1]))
+    
+    return calibration_values_sum
 
-print(calibration_values_sum)
+print(find_calibration_value(lines))
